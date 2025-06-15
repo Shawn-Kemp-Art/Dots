6d2b1d5e1900d22b334de7706c9974cc997f23bc
Shawn Kemp <shawn.kemp@gmail.com>
2025-06-15 13:34:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -582,6 +582,7 @@ document.addEventListener('keypress', (event) => {
                     }
                 } 
                 console.log("optimizing")
+                if z=stacks-1 {console.log("plottable")}
             }
         }
 
